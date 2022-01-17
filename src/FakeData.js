fad4e39346db1065924ae62d90a00f4031c3043c
sibTunechy <ayotemitope930@gmail.com>
2022-01-17 06:42:41 +0100

    first change

---
+++ src/FakeData.js
@@ -0,0 +1,52 @@
+export const UsersData = [
+    {
+        id: 1,
+        name: "Jerry West",
+        username: "Jeremiah",
+    },
+    {
+        id: 2,
+        name: "Wayne Carter",
+        username: "Lil wayne",
+    },
+    {
+        id: 3,
+        name: "Bruce Wayne",
+        username: "Batman",
+    },
+    {
+        id: 4,
+        name: "Clark Kent",
+        username: "Superman",
+    },
+    {
+        id: 5,
+        name: "Fillipo Inzaghi",
+        username: "Pipo",
+    },
+    {
+        id: 6,
+        name: "David Beckham",
+        username: "Becky Oloye",
+    },
+    {
+        id: 7,
+        name: "Tomi Human",
+        username: "Larondo",
+    },
+    {
+        id: 8,
+        name: "Akin Posh Cayenne",
+        username: "Asiwaju",
+    },
+    {
+        id: 9,
+        name: "Cristiano Ronaldo",
+        username: "Ororo",
+    },
+    {
+        id: 10,
+        name: "Sean Carter",
+        username: "Jay Z",
+    },
+];
